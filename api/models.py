1733708ebbb3a99b2e122f2ebeca2e7932cac045
Kaden Daya <kaden.daya@icloud.com>
2025-10-25 19:16:48 -0400

    feat: add PDF upload support to financial data API

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Text, ForeignKey, LargeBinary
+from sqlalchemy.orm import relationship
 from database import Base
 
 class User(Base):
@@ -6,3 +7,18 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
     password = Column(String)
+    financial_data = relationship("FinancialData", back_populates="user")
+
+class FinancialData(Base):
+    __tablename__ = "financial_data"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    credit_card_limit = Column(String)
+    card_age = Column(String)
+    credit_card_statement = Column(LargeBinary)
+    credit_forms = Column(Text)
+    current_debt = Column(String)
+    debt_amount = Column(String)
+    debt_end_date = Column(String)
+    debt_duration = Column(String)
+    user = relationship("User", back_populates="financial_data")
